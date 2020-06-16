648228a13031edd8652e2e7a59ad4a7c3e038f8c
Allan Farca <allan.farca@gmail.com>
2020-06-16 03:51:22 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,7 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+<<<<<<< HEAD
   belongs_to :network
 
   def actors_list
@@ -11,6 +12,15 @@ class Show < ActiveRecord::Base
       array << "#{actor.first_name} #{actor.last_name}"
     end
     array
+=======
+
+  def actors_list
+    Character.all? { |e|  }.each do |own|
+      character = Character.find(own.id)
+      actor = Actor.find(character.id)
+      "#{actor.first_name} #{actor.last_name}"
+    end
+>>>>>>> 01940ef9c166412d8dbcb61afe29f117b169c421
   end
 
 end
